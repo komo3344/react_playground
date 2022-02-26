987e5dfa0bb433ce1ca183a27437b6970fd5d4cb
junyoung <jy940608@naver.com>
2022-02-27 01:32:39 +0900

    Add: coin api test

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,37 +1,75 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDoList, setToDoList] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDoList((currentArray) => [toDo, ...currentArray]);
   };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  // console.log(setSelected);
+  // 투두리스트 연습
+  // const [toDo, setToDo] = useState("");
+  // const [toDoList, setToDoList] = useState([]);
+  // const onChange = (event) => setToDo(event.target.value);
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (toDo === "") {
+  //     return;
+  //   }
+  //   setToDo("");
+  //   setToDoList((currentArray) => [toDo, ...currentArray]);
+  // };
 
   return (
     <div>
-      <h1>My To Do List ({toDoList.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="할 일을 적어주세요"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ui>
-        {toDoList.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ui>
+      <h1> Coin List {loading ? "" : `(${coins.length})`}</h1>
+
+      {/* Loading */}
+      {loading ? (
+        <strong>"Loading..."</strong>
+      ) : (
+        <form onSubmit={onSubmit}>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        </form>
+      )}
     </div>
+
+    // 투두리스트 연습
+    // <div>
+    //   <h1>My To Do List ({toDoList.length})</h1>
+    //   <form onSubmit={onSubmit}>
+    //     <input
+    //       onChange={onChange}
+    //       value={toDo}
+    //       type="text"
+    //       placeholder="할 일을 적어주세요"
+    //     />
+    //     <button>Add To Do</button>
+    //   </form>
+    //   <hr />
+    //   <ui>
+    //     {toDoList.map((item, index) => (
+    //       <li key={index}>{item}</li>
+    //     ))}
+    //   </ui>
+    // </div>
   );
 }
 
