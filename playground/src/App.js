ef2991193464398bd49be73acafb256bbc3f5ed9
junyoung <jy940608@naver.com>
2022-02-28 22:30:31 +0900

    ADD: movie api

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,5 +1,44 @@
 import { useState, useEffect } from "react";
 
-function App() {}
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        "Loading.."
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt="Not found" />
+              <h2>{movie.title}</h2>
+              <p>{movie.description_full}</p>
+              <ul>
+                {movie.genres?.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+      ok
+    </div>
+  );
+}
 
 export default App;
