c42a26257c94c32a8042c0f2740b01cdb2353df8
junyoung <jy940608@naver.com>
2022-03-01 20:09:30 +0900

    Refact: 앱 구조 수정

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,44 +1,7 @@
 import { useState, useEffect } from "react";
-
+import Movie from "./components/Movie";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        "Loading.."
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt="Not found" />
-              <h2>{movie.title}</h2>
-              <p>{movie.description_full}</p>
-              <ul>
-                {movie.genres?.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-      ok
-    </div>
-  );
+  return null;
 }
 
 export default App;
