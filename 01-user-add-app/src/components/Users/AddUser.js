a8b22605494fae4afc20ffdf0b5aac52fde47c80
junyoung <jy940608@naver.com>
2022-09-04 21:57:31 +0900

    feat: useState 추가

--- 01-user-add-app/src/components/Users/AddUser.js
+++ 01-user-add-app/src/components/Users/AddUser.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import claasses from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const [enteredAge, setEnteredAge] = useState(0);
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
   const addUserHandler = (event) => {
     event.preventDefault();
   };
@@ -11,9 +19,19 @@ const AddUser = (props) => {
     <Card className={claasses.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username: </label>
-        <input type="text" id="username" />
+        <input
+          value={enteredUsername}
+          type="text"
+          id="username"
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Year)</label>
-        <input type="text" id="age" />
+        <input
+          value={enteredAge}
+          type="number"
+          id="age"
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
