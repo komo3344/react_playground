1da827637bd736549f733a119fc0a841550d3ac9
junyoung <jy940608@naver.com>
2022-09-04 21:49:00 +0900

    feat: UI card, button 추가

--- 01-user-add-app/src/components/Users/AddUser.js
+++ 01-user-add-app/src/components/Users/AddUser.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import claasses from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <Card>
+    <Card className={claasses.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username: </label>
         <input type="text" id="username" />
         <label htmlFor="age">Age (Year)</label>
         <input type="text" id="age" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
