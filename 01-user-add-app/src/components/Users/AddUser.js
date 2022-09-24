34a1ed5fb0d43a3f8b804f5fa30d6f43e92d7e5d
junyoung <jy940608@naver.com>
2022-09-25 00:00:30 +0900

    use portal

--- 01-user-add-app/src/components/Users/AddUser.js
+++ 01-user-add-app/src/components/Users/AddUser.js
@@ -43,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Wrapper>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -70,7 +70,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </Wrapper>
+    </>
   );
 };
 
