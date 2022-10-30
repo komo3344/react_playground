d064c56754265944499b4ac2302b5f36336f2b0e
junyoung <jy940608@naver.com>
2022-10-31 00:15:10 +0900

    feat: 동적 contextAPI 사용

--- 02-hooks/src/components/MainHeader/Navigation.js
+++ 02-hooks/src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from "../../store/auth-context";
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const ctx = useContext(AuthContext); // useContext훅에 context 포인터 전달
   return (
     <nav className={classes.nav}>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
         )}
         {ctx.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctx.onLogout}>Logout</button>
           </li>
         )}
       </ul>
