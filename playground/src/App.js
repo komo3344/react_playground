b0d0071d317e6a8180ecc9700b67aee818a9d145
junyoung <jy940608@naver.com>
2023-03-11 11:39:17 +0900

    init playground

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
-import Navigation from "./components/Navigation";
+
 function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movie/:id" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>hello</div>;
 }
 
 export default App;
