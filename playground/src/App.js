874413cab6f161e22642c7789135ab858009ec8d
junyoung <jy940608@naver.com>
2023-03-12 22:18:18 +0900

    simple todo list

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>hello</div>;
-}
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  const deleteBtn = (e) => {
+    const index = parseInt(e.target.className);
+    setToDos((currentArray) =>
+      currentArray.filter((item, todoIndex) => index !== todoIndex)
+    );
+  };
 
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <div>
+            <li key={index}>
+              {item}
+              <button className={index} onClick={deleteBtn}>
+                ❌
+              </button>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
